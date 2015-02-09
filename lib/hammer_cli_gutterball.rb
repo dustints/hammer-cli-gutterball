46e3f4c50c789f74cc9abca3f6a5c493de43c58d
Adam Price <komidore64@gmail.com>
2015-02-09 13:16:32 -0500

    forgot to require hcli-katello bits

--- lib/hammer_cli_gutterball.rb
+++ lib/hammer_cli_gutterball.rb
@@ -1,4 +1,11 @@
+# need dat katello yo
+require 'hammer_cli_katello'
+
+# base commands
+require 'hammer_cli_gutterball/commands'
+
+# commands
 HammerCLI::MainCommand.lazy_subcommand('content-report',
   _('View Content Reports'),
   'HammerCLIGutterball::ContentReportsCommand',
-  'hammer_cli_gutterball/content_reports')
+  'hammer_cli_gutterball/content_report')
